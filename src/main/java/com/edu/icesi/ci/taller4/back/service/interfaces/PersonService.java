19f73388c71ad526af22c34fd4643019d12a18dc
backtojuan <juanchouni@hotmail.com>
2021-06-18 01:13:42 -0500

    Solved merged texts

--- src/main/java/com/edu/icesi/ci/taller4/back/service/interfaces/PersonService.java
+++ src/main/java/com/edu/icesi/ci/taller4/back/service/interfaces/PersonService.java
@@ -1,10 +1,6 @@
 package com.edu.icesi.ci.taller4.back.service.interfaces;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
-public interface PersonService {
->>>>>>> 25644786973967410a755f142756a77da358ab9c
 
 import com.edu.icesi.ci.taller4.back.model.Person;
 import com.edu.icesi.ci.taller4.back.model.PersonRole;
